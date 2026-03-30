8157b5a45f10d5a197653887987852461f796958
abubakarsaeed1112 <abubakarsaeed1112@gmail.com>
2026-03-30 20:37:23 +0500

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,13 @@ FROM python:3.9
 WORKDIR /app/backend
 
 COPY requirements.txt /app/backend
+
+# Install system dependencies
 RUN apt-get update \
     && apt-get upgrade -y \
-    && apt-get install -y gcc default-libmysqlclient-dev pkg-config \
+    # added default-mysql-client to support mysqladmin command used for DB readiness check
+    && apt-get install -y gcc default-libmysqlclient-dev pkg-config default-mysql-client \
     && rm -rf /var/lib/apt/lists/*
-
 
 # Install app dependencies
 RUN pip install mysqlclient
@@ -16,6 +18,9 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app/backend
 
 EXPOSE 8000
+
+# using runserver for simplicity (can be replaced with gunicorn in production)
 CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
-#RUN python manage.py migrate
-#RUN python manage.py makemigrations
+
+# RUN python manage.py migrate
+# RUN python manage.py makemigrations
--- api/views.py
+++ api/views.py
@@ -31,16 +31,17 @@ def getRoutes(request):
             'Endpoint': '/notes/id/update/',
             'method': 'PUT',
             'body': {'body': ""},
-            'description': 'Creates an existing note with data sent in post request'
+            'description': 'Updates an existing note'
         },
         {
             'Endpoint': '/notes/id/delete/',
             'method': 'DELETE',
             'body': None,
-            'description': 'Deletes and exiting note'
+            'description': 'Deletes an existing note'
         },
     ]
     return Response(routes)
+
 
 @api_view(['GET'])
 def getNotes(request):
@@ -48,11 +49,13 @@ def getNotes(request):
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getNote(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
 
 @api_view(['PUT'])
 def updateNote(request, pk):
@@ -62,17 +65,19 @@ def updateNote(request, pk):
         serializer.save()
     return Response(serializer.data)
 
+
 @api_view(['DELETE'])
 def deleteNote(request, pk):
     note = Note.objects.get(id=pk)
     note.delete()
     return Response('Note was deleted!')
 
+
 @api_view(['POST'])
 def createNote(request):
     data = request.data
     note = Note.objects.create(
-        body=data['body']
+        body=data.get('body', '')  # fixed: avoid KeyError if body missing
     )
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
